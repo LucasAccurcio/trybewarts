4808e9a722abb365dce7b768ea5802fc7c1e357c
Cezar <cezarguimaraes@gmail.com>
2021-07-21 18:45:03 -0300

    requisito 14

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let getSenha = document.getElementById('input-senha').value;
 
 function buttonAlert(event) {
     event.preventDefault();
-    let loginPadrao = 'adimin';
-    let senhaPadrao = 'adimin';
+    let loginPadrao = 'tryber@teste.com';
+    let senhaPadrao = '123456';
     let getLogin = document.getElementById('input-login').value;
     let getSenha = document.getElementById('input-senha').value;
     
     if( getLogin !== loginPadrao || getSenha !== senhaPadrao){
-        alert('Login ou senha inválidos');
+        alert('Login ou senha inválidos.');
     } else if (getLogin === loginPadrao && getSenha === senhaPadrao){
         alert('Olá, Tryber!');
     }
