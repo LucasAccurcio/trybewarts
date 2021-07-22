fb1549c80c60931913db37f9172e45125ba3f562
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-07-21 21:58:07 -0300

    Corrige espaçamentos com o ESLint e remove 2 variáveis chamadas em duplicidade no arquivo script.js

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
-let getButton = document.getElementById('enter-button');
-let getLogin = document.getElementById('input-login').value;
-let getSenha = document.getElementById('input-senha').value;
-
-
+const getButton = document.getElementById('enter-button');
 
 function buttonAlert(event) {
-    event.preventDefault();
-    let loginPadrao = 'tryber@teste.com';
-    let senhaPadrao = '123456';
-    let getLogin = document.getElementById('input-login').value;
-    let getSenha = document.getElementById('input-senha').value;
-    
-    if( getLogin !== loginPadrao || getSenha !== senhaPadrao){
-        alert('Login ou senha inválidos.');
-    } else if (getLogin === loginPadrao && getSenha === senhaPadrao){
-        alert('Olá, Tryber!');
-    }
+  event.preventDefault();
+  const loginPadrao = 'tryber@teste.com';
+  const senhaPadrao = '123456';
+  const getLogin = document.getElementById('input-login').value;
+  const getSenha = document.getElementById('input-senha').value;
+
+  if (getLogin !== loginPadrao || getSenha !== senhaPadrao) {
+    alert('Login ou senha inválidos.');
+  } else if (getLogin === loginPadrao && getSenha === senhaPadrao) {
+    alert('Olá, Tryber!');
+  }
 }
 getButton.addEventListener('click', buttonAlert);
 
-
-let btnSubmit = document.getElementById('submit-btn');
-let getCheckbox = document.getElementById('agreement');
+const btnSubmit = document.getElementById('submit-btn');
+const getCheckbox = document.getElementById('agreement');
 btnSubmit.disabled = true;
 function stateHandle(event) {
-    event.preventDefault();
-    if (getCheckbox.value === "") {
-        btnSubmit.disabled = true;
-        getCheckbox.value = true 
-        
-    } else {
-        btnSubmit.disabled = false;
-        getCheckbox.value = "";
-    }
-    console.log(getCheckbox.value);
+  event.preventDefault();
+  if (getCheckbox.value === '') {
+    btnSubmit.disabled = true;
+    getCheckbox.value = true;
+  } else {
+    btnSubmit.disabled = false;
+    getCheckbox.value = '';
+  }
+  console.log(getCheckbox.value);
 }
-getCheckbox.addEventListener("change", stateHandle);
-
-
+getCheckbox.addEventListener('change', stateHandle);
